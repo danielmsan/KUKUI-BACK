525ac616dfa267aeb448e32492a1dff2b7271301
urielspv <urielperez9781@gmail.com>
2023-04-03 14:24:48 -0600

    paquetes

--- back-kukui/src/main/java/com/kukui/BackKukuiApplication.java
+++ back-kukui/src/main/java/com/kukui/BackKukuiApplication.java
@@ -1,13 +1,13 @@
-package com.kukui.KUKUIMID;
+package com.kukui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KukuiMidApplication {
+public class BackKukuiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(KukuiMidApplication.class, args);
+		SpringApplication.run(BackKukuiApplication.class, args);
 	}
 
 }
--- back-kukui/src/test/java/com/kukui/BackKukuiApplicationTests.java
+++ back-kukui/src/test/java/com/kukui/BackKukuiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.kukui.KUKUIMID;
+package com.kukui;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KukuiMidApplicationTests {
+class BackKukuiApplicationTests {
 
 	@Test
 	void contextLoads() {
