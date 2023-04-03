8bdc3d3b3926ab0a89eca5a2c26f35dfe1a74639
urielspv <urielperez9781@gmail.com>
2023-04-03 12:40:27 -0600

    Estructura java

---
+++ src/main/java/com/kukui/KUKUIMID/KukuiMidApplication.java
@@ -0,0 +1,13 @@
+package com.kukui.KUKUIMID;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KukuiMidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KukuiMidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kukui/KUKUIMID/KukuiMidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kukui.KUKUIMID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KukuiMidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
